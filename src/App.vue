25a36eec10270b80e0586f9e417655541d4eb707
Chris LoJacono <chrislojacono@quavered.com>
2021-12-09 11:47:19 -0600

    basic functionality on list of todos

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    
+    <Todos v-bind:todos='todos' v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-
+    Todos
   },
   data() {
     return {
      todos: [
-       
+       {
+         id: 1,
+         title: "Todo One",
+         completed: false
+       },
+        {
+         id: 2,
+         title: "Todo Two",
+         completed: true
+       },
+       {
+         id: 3,
+         title: "Todo Three",
+         completed: false
+       },
      ]
   }
+ },
+ methods: {
+   deleteTodo(id) {
+     this.todos = this.todos.filter(todo => todo.id !== id);
+   }
  }
 }
 </script>
